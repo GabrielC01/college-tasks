e6bba6ab38b08e8118de664a48f1ad4d334e1ae2
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-04-10 20:37:10 +0000

    Moved files to appropriate directory.

---
+++ data-structures1/task1/arrays/e2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+	short integers[6], i;
+	size_t lenIntegers = sizeof(integers) / sizeof(integers[0]);
+
+	printf("Entre com seis inteiros:\n");
+	for (i = 0; i < lenIntegers; i++)
+		scanf("%hd", &integers[i]);
+
+	for (i = 0; i < lenIntegers; i++)
+		printf("%hd ", integers[i]);
+	printf("\n");
+}
