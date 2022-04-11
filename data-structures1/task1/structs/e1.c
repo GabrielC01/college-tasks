7152266bcafc3c68b302097347df89f06f067f8e
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-04-11 17:50:57 +0000

    Exercise 1.

--- data-structures1/task1/structs/e1.c
+++ data-structures1/task1/structs/e1.c
@@ -1,14 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-struct students {
-	char * name;
+typedef struct students {
+	char name[32];
 	unsigned short regNumber;
 	float grades[2];
-};
+	float avg;
+} Students;
 
 void main()
 {
-	struct students al;
+	Students * bcc;
+	unsigned short numStudents;
 
+	// Read in number of students to be registered
+	printf("Quantos alunos deseja cadastrar?\n");
+	scanf("%hu", &numStudents);
+
+	// Allocate the appropriate space in memory for their data
+	bcc = calloc(sizeof(bcc), numStudents * sizeof(Students));
 	
+	// Register their data
+	for (unsigned short i = 0; i < numStudents; i++)
+	{
+		printf("*ALUNO %hu*\n", i+1);
+		printf("•Nome:\n");
+		scanf("%s", &bcc[i].name);
+		printf("•Matricula:\n");
+		scanf("%hu", &bcc[i].regNumber);
+		printf("•Notas:\n");
+		scanf("%f %f", &bcc[i].grades[0], &bcc[i].grades[1]);
+		bcc[i].avg = (bcc[i].grades[0] + bcc[i].grades[1]) / 2;
+	}
+
+	// Print the name, registration number and average grade of each student
+	for (unsigned short i = 0; i < numStudents; i++)
+	{
+		printf("(Matricula: %hu, Nome: \"%s\", Media: %.1f)\n", bcc[i].regNumber, bcc[i].name, bcc[i].avg);
+	}
+
+	free(bcc);
 }
