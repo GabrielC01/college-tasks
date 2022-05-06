857af739c6de3761557298ae98a210cc1fd7a761
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-05-06 18:16:44 -0300

    Main version.

--- computer-organization/task6/memory_access.asm
+++ computer-organization/task6/memory_access.asm
@@ -5,17 +5,23 @@
 ; é o procedimento mmu.
 
 ;; Constantes:
-SYS_EXIT 	equ 1		; cód. de saída 	 (Linux x86)
-SYS_WRITE 	equ 4		; cód. de escrita	 (Linux x86)
-MEM_SIZE 	equ 1024	; Tamanho da memória
-MREAD 		equ 0		; Leitura da memória (modo)
-MWRITE		equ 1		; Escrita na memória (modo)
+SYS_EXIT 	equ 1		; cód. de saída 	 	(Linux x86)
+SYS_WRITE 	equ 4		; cód. de escrita	 	(Linux x86)
+STD_OUT		equ 1		; cód. da saída padrão	(Linux x86)
+MEM_SIZE 	equ 16		; Tamanho da memória
+MREAD 		equ 0		; Leitura da memória 	(modo)
+MWRITE		equ 1		; Escrita na memória 	(modo)
 
 
 ;; Subprograma principal:
 section .text
 	global _start
-_start
+_start:
+    call print_RAM_card
+
+    mov eax, SYS_EXIT
+    mov ebx, 0
+    int 0x80
 
 
 ;; Outros subprogramas:
@@ -24,16 +30,25 @@ mmu:
 	ret
 
 ; Imprime o pente de memória (tabela com seus elementos)
-printRAMCard:
+print_RAM_card:
+    mov ecx, MEM_SIZE;
+    print_elements:
+        push ecx
+        mov ebx, STD_OUT
+        mov ecx, RAM
+        mov edx, 1
+        int 0x80
+        pop ecx
+        loop print_elements
 	ret
 
 
-;; Seções de declaração variáveis
+;; Seções de declaração de variáveis
 section .data
 	RAM times MEM_SIZE dw 0 ; Memória zerada
 
 section .bss
 	; Argumentos do procedimento mmu:
-	mmuArg0 resb	; Modo
-	mmuArg1 resw	; Endereço de memória
-	mmuArg3 resw	; Dado
+	mmuArg0 resb 1	; Modo
+	mmuArg1 resw 1	; Endereço de memória
+	mmuArg3 resw 1	; Dado
