2489f2212af1bebab992137ff49a0e7465b77a12
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-11 11:18:14 -0300

    Fixed commentary.

--- computer_architecture/fibo.rs
+++ computer_architecture/fibo.rs
@@ -1,5 +1,5 @@
 // Func. obter o n-ésimo elemento da sequência de Fibonacci,
-// considerando, neste caso, que se inicia em 1.
+// considerando, neste caso, que se inicia em 0.
 fn fibo(nth: i32) -> i64 {
     let mut a = 0;
     let mut b = 1;
