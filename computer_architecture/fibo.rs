8bd3b66d6c46c5364dc3a49bba1a9c7350d6ac68
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-11 10:22:40 -0300

    Found another way to solve.

--- computer_architecture/fibo.rs
+++ computer_architecture/fibo.rs
@@ -1,11 +1,11 @@
 // Func. obter o n-ésimo elemento da sequência de Fibonacci,
 // considerando, neste caso, que se inicia em 1.
 fn fibo(nth: i32) -> i64 {
-    if nth <= 1 {
-        return 1;
-    } else {
-        return fibo(nth - 1) + fibo(nth - 2);
+    let mut n = 0;
+    for i in 2..=nth {
+        n = i - 1 + i - 2;
     }
+    n.into()
 }
 
 fn main() {
