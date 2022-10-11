b68d6bfe01b6a736f90bccb940a65b817fa0fc60
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-11 10:12:15 -0300

    Fixed.

--- computer_architecture/fibo.rs
+++ computer_architecture/fibo.rs
@@ -1,19 +1,7 @@
-use std::io;
-
-// Proc. ler inteiro da entrada padrão
-fn read_int() -> Result<i32, bool> {
-    let sc = io::stdin();
-    let mut str_input = String::new();
-    sc.read_line(&mut str_input);
-    return match str_input.parse::<i32>() {
-        Ok(n) => Ok(n),
-        Err(..) => Err(false),
-    }
-}
-
-// Func. obter o n-ésimo elemento da sequência de Fibonacci
+// Func. obter o n-ésimo elemento da sequência de Fibonacci,
+// considerando, neste caso, que se inicia em 1.
 fn fibo(nth: i32) -> i64 {
-    if nth <= 2 {
+    if nth <= 1 {
         return 1;
     } else {
         return fibo(nth - 1) + fibo(nth - 2);
@@ -21,5 +9,5 @@ fn fibo(nth: i32) -> i64 {
 }
 
 fn main() {
-    println!("{}", fibo(16));
+    println!("{}", fibo(4));
 }
