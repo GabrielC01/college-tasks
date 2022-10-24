aaa616f2be5f9e1b6e8ade2987199cfc46f1025e
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-24 16:08:30 -0300

    Tratamento de chave nula.

--- data-structures2/task2/q1.c
+++ data-structures2/task2/q1.c
@@ -10,30 +10,46 @@ size_t hash(size_t ht_length, unsigned int key) {
 // Inserir registro na tabela.
 int insert(unsigned int ht[], size_t ht_length, unsigned int key) {
 	int i = (int) hash(ht_length, key);
-	if (ht[i] == 0) {
-		ht[i] = key;
-	} else if (ht[i] != key) {
-		size_t j = (i + 1) % ht_length;
-		while (j != i && ht[j] != 0) {
-			j = (j + 1) % ht_length;
+	if (key != 0) {
+		if (ht[i] == 0) {
+			ht[i] = key;
+		} else if (ht[i] != key) {
+			size_t j = (i + 1) % ht_length;
+			while (j != i && ht[j] != 0) {
+				j = (j + 1) % ht_length;
+			}
+			if (j == i) {
+				i = -1;
+			} else {
+				i = j;
+				ht[j] = key;
+			}
 		}
-		if (j == i) {
-			i = -1;
-		} else {
-			i = j;
-			ht[j] = key;
-		}
+	} else {
+		i = -1;
 	}
 	return i;
 }
 
 // Remover registro da tabela.
 int rm(unsigned int ht[], size_t ht_length, unsigned int key) {
-	if (key == 0) return -1; // Registro vazio.
-							 //
 	int i = (int) hash(ht_length, key);
-	if (ht[i] == key) {
-		ht[i] = 0;
+	if (key != 0) {
+		if (ht[i] == key) {
+			ht[i] = 0;
+		} else {
+			size_t j = (i + 1) % ht_length;
+			while (j != i && ht[j] != 0) {
+				j = (j + 1) % ht_length;
+			}
+			if (j == i || ht[j] == 0) {
+				i = -1;
+			} else {
+				i = j;
+				ht[j] = 0;
+			}
+			i = -1;
+		}
 	} else {
 		i = -1;
 	}
@@ -55,17 +71,21 @@ int main() {
 	// a) Inserção dos registros 4, 17, 13, 35, 25, 11, 2, 10, 32.
 	unsigned int test[] = { 4, 17, 13, 35, 25, 11, 2, 10, 32 };
 	size_t length_test = sizeof(test) / sizeof(test[0]);
+	printf("a) ");
 	for (size_t i = 0; i < length_test; i++) {
 		insert(ht, ht_length, test[i]);
 	}
 	printHt(ht, ht_length);
 	printf("\n");
 	
-	// // b) Remoção dos registros 25 e 11.
-	// rm(ht, ht_length, 25);
-	// rm(ht, ht_length, 11);
-	// 
-	// // c) Inserção dos registros 40 e 3.
+	// b) Remoção dos registros 25 e 11.
+	rm(ht, ht_length, 25);
+	rm(ht, ht_length, 11);
+	printf("b) ");
+	printHt(ht, ht_length);
+	printf("\n");
+	
+	// c) Inserção dos registros 40 e 3.
 	// insert(ht, ht_length, 40); insert(ht, ht_length, 3);
 
 	return 0;
