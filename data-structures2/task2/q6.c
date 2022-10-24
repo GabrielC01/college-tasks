5f6a74c482b79d07420229b88dd454bee965aa17
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-24 18:04:51 -0300

    Criação de pilha (lista simplesmente encadeada); inserção de elemento da pilha; remoção de elemento da pilha.

---
+++ data-structures2/task2/q6.c
@@ -0,0 +1,37 @@
+/* Implementação de uma tabela de dispersão usando endereçamento fechado, isto
+ * é, encadeamento de colisões. */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+// Pilha (lista simplesmente encadeada).
+typedef struct StackNode StackNode;
+
+struct StackNode {
+	unsigned int data;
+	StackNode* next;
+};
+
+
+// Inicializar nova pilha.
+StackNode* newStack() {
+	return (StackNode*) malloc(sizeof(StackNode));
+}
+
+
+// Inserir elemento à pilha.
+void addToStack(StackNode* top, unsigned int data) {
+	StackNode* new_node = (StackNode*) malloc(sizeof(StackNode));
+	new_node->data = data;
+	new_node->next = NULL;
+	top->next = new_node;
+}
+
+// Remover topo da pilha.
+void pop(StackNode* top) {
+	top = NULL;
+}
+
+int main() {
+	return 0;
+}
