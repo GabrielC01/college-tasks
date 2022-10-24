005310243177839ffc791e5d619d6d486d65f192
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-24 14:50:26 -0300

    Inserção na tabela hash.

---
+++ data-structures2/task2/q1.c
@@ -0,0 +1,31 @@
+/* Implementação de uma tabela de dispersão usando endereçamento aberto,
+ * especificamente a sondagem linear. O registro nulo é o 0. */
+#include <stdio.h>
+
+// Função de dispersão.
+size_t hash(size_t ht_length, unsigned int key) {
+	return key % ht_length;
+}
+
+// Inserir registro na tabela.
+int insert(unsigned int ht[], size_t ht_length, unsigned int key) {
+	int i = (int) hash(ht_length, key);
+	if (ht[i] == 0) {
+		ht[i] = key;
+	} else if (ht[i] != key) {
+		size_t j = (i + 1) % ht_length;
+		while (j != i && ht[j] != 0) {
+			j = (i + 1) % ht_length;
+		}
+		j == i ? i = -1 : i = j;
+	}
+	return i;
+}
+
+// Remover registro da tabela.
+int remove(unsigned int ht[], size_t ht_length, unsigned int key) {
+}
+
+int main() {
+	return 0;
+}
