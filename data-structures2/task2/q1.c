43627825d2e25517b5fe0d95f3f13d654117a497
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-24 15:21:31 -0300

    Correção da fórmula de sondagem linear.

--- data-structures2/task2/q1.c
+++ data-structures2/task2/q1.c
@@ -15,15 +15,15 @@ int insert(unsigned int ht[], size_t ht_length, unsigned int key) {
 	} else if (ht[i] != key) {
 		size_t j = (i + 1) % ht_length;
 		while (j != i && ht[j] != 0) {
-			j = (i + 1) % ht_length;
+			j = (j + 1) % ht_length;
 		}
-		j == i ? i = -1 : i = j;
+		i = (j == i) ? -1 : j;
 	}
 	return i;
 }
 
 // Remover registro da tabela.
-int remove(unsigned int ht[], size_t ht_length, unsigned int key) {
+int rm(unsigned int ht[], size_t ht_length, unsigned int key) {
 	if (key == 0) return -1; // Registro vazio.
 							 //
 	int i = (int) hash(ht_length, key);
@@ -36,7 +36,7 @@ int remove(unsigned int ht[], size_t ht_length, unsigned int key) {
 }
 
 // Imprimir tabela.
-void printHt(unsigned int ht, size_t ht_length) {
+void printHt(unsigned int ht[], size_t ht_length) {
 	for (size_t i = 0; i < ht_length; i++) {
 		printf("%u ", ht[i]);
 	}
@@ -46,10 +46,22 @@ void printHt(unsigned int ht, size_t ht_length) {
 int main() {
 	size_t ht_length = 11;
 	unsigned int ht[ht_length];
+
+	// a) Inserção dos registros 4, 17, 13, 35, 25, 11, 2, 10, 32.
 	unsigned int test[] = { 4, 17, 13, 35, 25, 11, 2, 10, 32 };
 	size_t length_test = sizeof(test) / sizeof(test[0]);
 	for (size_t i = 0; i < length_test; i++) {
 		insert(ht, ht_length, test[i]);
 	}
+	printHt(ht, ht_length);
+	printf("\n");
+	
+	// // b) Remoção dos registros 25 e 11.
+	// rm(ht, ht_length, 25);
+	// rm(ht, ht_length, 11);
+	// 
+	// // c) Inserção dos registros 40 e 3.
+	// insert(ht, ht_length, 40); insert(ht, ht_length, 3);
+
 	return 0;
 }
